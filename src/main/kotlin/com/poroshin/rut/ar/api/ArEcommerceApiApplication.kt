41198503b0aab1e97d19836f8dcf3aa353fb498b
Poroshin Gleb <gleb.poroshin@lemanapro.ru>
2025-11-04 13:22:15 +0300

    [infra] Initialize Gradle project

---
+++ src/main/kotlin/com/poroshin/rut/ar/api/ArEcommerceApiApplication.kt
@@ -0,0 +1,11 @@
+package com.poroshin.rut.ar.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArEcommerceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArEcommerceApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/poroshin/rut/ar/api/ArEcommerceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.poroshin.rut.ar.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArEcommerceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
